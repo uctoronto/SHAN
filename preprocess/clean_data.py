9f0c1fd5216dbf328cadf5afbd6f06e60035d05c
CallmeMrhu <mrhu666@163.com>
2018-08-20 10:49:32 +0800

    commit

--- preprocess/clean_data.py
+++ preprocess/clean_data.py
@@ -1,6 +1,5 @@
 import pandas as pd
 
-
 # 该py文件的功能是将源数据
 # first_step_clean_data方法：
 # 1、去除多余列；2、仅仅保留最近n个月的数据；3、只保留至少被m个用户购买过的item。
@@ -40,21 +39,12 @@ class Dataset(object):
 
         # 统计每个ite_ID被多少个不同的user购买，只保留被至少appear_time个用户购买过的item
         ds = self.item_appear(appear_time, ds)
-        # dm = ds[['use_ID', 'ite_ID']].drop_duplicates()
-        # da = dm.groupby(by=['ite_ID'], as_index=False)['ite_ID'].agg({'cnt': 'count'})
-        # ite_list = list(da[da['cnt'] >= appear_time]['ite_ID'])
-        # ds = ds[ds['ite_ID'].isin(ite_list)]
 
         # 对每个用户的消费行为，如果一个时间点只有一个item的数据，则去除，即只保留重复数据
         ds = self.session_not_single(ds)
-        # ds = ds[ds.duplicated(['use_ID', 'time'], keep=False) == True]
 
         # 对于每个用户，如果只存在一个session，则去除
         ds = self.user_have_more_session(ds,user_sessin)
-        # dm = ds.drop_duplicates(['use_ID', 'time'])
-        # da = dm.groupby(by=['use_ID'], as_index=False)['use_ID'].agg({'cnt': 'count'})
-        # use_list = list(da[da['cnt'] >= 6]['use_ID'])
-        # ds = ds[ds['use_ID'].isin(use_list)]
 
         # ds = self.item_appear(appear_time, ds)
         ds.to_csv('~/SHAN/data.csv', index=False)
